44e3687e86210320dfc53cad81b0a99c9a56cf8d
Marina Murashev <marinamurashev@gmail.com>
2014-09-14 20:35:39 -0500

    sets up devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { host: 'marinamurashev.herokuapp.com' }
 end
