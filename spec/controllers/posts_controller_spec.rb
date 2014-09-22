97a84e11c8cc89f546bbf867cacb49f25896a9e1
Marina Murashev <marinamurashev@gmail.com>
2014-09-21 20:08:05 -0500

    successfully loads users and posts factories when specs are run

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 describe PostsController do
+  describe "GET#show" do
+
+  end
+
+  describe "GET#index" do
+
+  end
 
 end
--- spec/models/post_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-describe Post do
-  it "has a valid factory" do
-    post = build :post
-    expect(post).to be_valid
-  end
-end
