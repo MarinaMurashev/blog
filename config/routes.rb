9ec742bf70eec117c92731f9e128cf410818b8d4
Marina Murashev <marinamurashev@gmail.com>
2014-09-28 20:37:25 -0500

    adds a static About page

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, only: :sessions
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
   root "posts#index"
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  get '/about' => 'pages#about'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
