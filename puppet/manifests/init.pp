5e435c8065e4c768d84d279408c8facb8838f181
Marina Murashev <marinamurashev@gmail.com>
2014-11-11 08:58:50 -0600

    adds to path variable, apt-update, bundle install, and libpq-dev & nodejs packages to box

--- puppet/manifests/init.pp
+++ puppet/manifests/init.pp
@@ -1,3 +1,15 @@
+Exec { path => "/home/vagrant/bin:/usr/local/rbenv/bin:/usr/local/rbenv/shims::/usr/local/rbenv/shims/bin:/usr/bin:/bin:/usr/sbin:/sbin" }
+
+exec { 'apt-update':
+  command => "/usr/bin/apt-get update",
+  onlyif => "/bin/bash -c 'exit $(( $(( $(date +%s) - $(stat -c %Y /var/lib/apt/lists/$( ls /var/lib/apt/lists/ -tr1|tail -1 )) )) <= 604800 ))'"
+}
+
+Exec["apt-update"] -> Package <| |>
+
+package { "libpq-dev": ensure => present }
+package { "nodejs": ensure => present }
+
 # --- Postgres -----------------------------------------------------------------
 class { 'postgresql::server': }
 
@@ -22,3 +34,13 @@ file { "/home/vagrant/.bundle":
   ensure => directory,
   owner => "vagrant"
 }
+
+exec {"bundle install":
+  command   => "bundle --binstubs=/home/vagrant/bin --path=/home/vagrant/.bundle",
+  cwd       => "/vagrant",
+  user      => "vagrant",
+  timeout   => 0,
+  require   => [Rbenv::Build["2.1.1"], File["/home/vagrant/bin"], File["/home/vagrant/.bundle"]]
+}
+
+Package <| |> -> Exec["bundle install"]
