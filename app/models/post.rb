f13591f544f9dea68d5a7959fa1f4af382cc5a6f
Marina Murashev <marinamurashev@gmail.com>
2014-11-24 21:56:46 -0800

    implements truncation of post body on index page

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,10 @@
 class Post < ActiveRecord::Base
+
+  PREVIEW_BREAK = "<!--- truncate --->"
+
   validates_presence_of :title, :body
   validates_uniqueness_of :title, :body
+  validates :body, format: { with: /#{PREVIEW_BREAK}/,
+    message: "must contain the truncation indicator #{PREVIEW_BREAK}" }
 
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 describe Post do
   describe "#valid" do
+    let(:truncate) { Post::PREVIEW_BREAK }
+
     it "has a non-empty title" do
       post = build :post, title: ""
       expect(post).to_not be_valid
@@ -18,8 +20,16 @@ describe Post do
     end
 
     it "has a unique body" do
-      post1 = create :post, body: "test"
+      post1 = create :post, body: "test #{truncate}"
+      expect(build :post, body: "test #{truncate}").to_not be_valid
+    end
+
+    it "is invalid when it doesn't contain Post's PREVIEW_BREAK" do
       expect(build :post, body: "test").to_not be_valid
+    end
+
+    it "is valid when it contains Post's PREVIEW_BREAK" do
+      expect(build :post, body: "test #{truncate}").to be_valid
     end
   end
 end
