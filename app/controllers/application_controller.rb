05292319ca4a13d4e41ff440d10a693633e61885
Marina Murashev <marinamurashev@gmail.com>
2014-11-25 12:39:18 -0800

    Revert "redirects full path with www subdomain"
    This reverts commit 7c1b07be9a8b38cba089be9ea962af25a6b5dea0.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,6 @@
 class ApplicationController < ActionController::Base
+  include EnsureDomain
+
   protect_from_forgery with: :exception
   before_action :authenticate_user!
-  before_filter :ensure_domain
-
-  private
-
-  def ensure_domain
-    redirect_to subdomain: 'www' unless Rails.env == "development"
-  end
 end
