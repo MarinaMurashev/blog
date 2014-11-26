176ce8f41193ecf1daf2c7d710198511ecfb1562
Marina Murashev <marinamurashev@gmail.com>
2014-11-26 11:53:05 -0800

    makes tweaks to sitemap generator config

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -3,10 +3,10 @@ require 'sitemap_generator'
 
 SitemapGenerator::Sitemap.default_host = 'http://www.marinamurashev.com'
 SitemapGenerator::Sitemap.create do
-  add '/posts', changefreq: 'daily', priority: 0.9
-  add '/about', changefreq: 'weekly'
+  add '/posts', changefreq: 'always', priority: 0.5
+  add '/about', changefreq: 'always', priority: 0.4
   Post.find_each do |post|
-    add post_path(post), lastmod: post.updated_at
+    add "/posts/#{post.id}", lastmod: post.updated_at, priority: 0.9
   end
 end
 SitemapGenerator::Sitemap.ping_search_engines
