f08b52f3245e5c34ffd7f86ba3995a959f33383b
Marina Murashev <marinamurashev@gmail.com>
2014-12-02 14:33:10 -0800

    makes host for sitemap dynamic based on where it's called from

--- app/controllers/sitemaps_controller.rb
+++ app/controllers/sitemaps_controller.rb
@@ -2,7 +2,7 @@ class SitemapsController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
-    @host = request.host
+    @host = "#{request.protocol}#{request.host}"
     @posts = Post.all
   end
 end
